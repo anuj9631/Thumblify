daafd25a781b6630a7ff7b17c60bc7bcb7bd7a8f
anuj9631 <anuj98426@gmail.com>
2026-01-10 17:58:45 +0530

    pages stureures

---
+++ client/src/pages/MyGeneration.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyGeneration = () => {
+  return <div>MyGeneration</div>;
+};
+
+export default MyGeneration;
---
+++ client/src/pages/YtPreview.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const YtPreview = () => {
+  return <div>YtPreview</div>;
+};
+
+export default YtPreview;
