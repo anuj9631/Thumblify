55a3155bfd3ad05e5f668378ff325bcac64c8c6a
anuj9631 <anuj98426@gmail.com>
2026-01-31 19:29:40 +0530

    updated user schema login functionality compelted

--- server/models/User.ts
+++ server/models/User.ts
@@ -11,6 +11,7 @@ export interface Iuser extends Document {
 const UserSchema = new mongoose.Schema<Iuser>({
   name: {type: String, required: true, trim: true},
   email: {type: String, required: true, trim: true, unique: true, lowercase: true},
+  password: {type:String, required: true}
 
 },{timestamps: true})
 
