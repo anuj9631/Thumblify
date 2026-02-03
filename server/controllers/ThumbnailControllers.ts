cfbe7d78063ed9516eb3750be787683cd3c91b88
anuj9631 <anuj98426@gmail.com>
2026-02-03 15:45:15 +0530

    created ai config and controller for generating image

--- server/controllers/ThumbnailControllers.ts
+++ server/controllers/ThumbnailControllers.ts
@@ -1,28 +1,61 @@
 
+
 import Thumbnail from "../models/Thumbnail.js";
 import { Request, Response } from "express"
+import { GenerateContentConfig, HarmBlockThreshold, HarmCategory } from '@google/genai';
 
-export const generateThumbnail = async (req: Request, res:Response) => {
+export const generateThumbnail = async (req: Request, res: Response) => {
   try {
-    const {userId} = req.session;
+    const { userId } = req.session;
     const {
-      title, prompt: user_prompt,style,aspect_ratio,color_scheme,text_overlay
+      title, prompt: user_prompt, style, aspect_ratio, color_scheme, text_overlay
     } = req.body;
 
-    const thumbnail=await Thumbnail.create({
-         userId,
-         title,
-         prompt_used:user_prompt,
-         user_prompt,
-         style,
-         aspect_ratio,
-         color_scheme,
-         text_overlay,
-         isGenerating : true
+    const thumbnail = await Thumbnail.create({
+      userId,
+      title,
+      prompt_used: user_prompt,
+      user_prompt,
+      style,
+      aspect_ratio,
+      color_scheme,
+      text_overlay,
+      isGenerating: true
     })
+   
+    const model = 'gemini-3-pro-image-preview'
+    const generationConfig : GenerateContentConfig ={
+       maxOutputTokens: 32768,
+       temperature: 1,
+       topP: 0.95,
+       responseModalities: ['IMAGE'],
+       imageConfig:{
+        aspectRatio: aspect_ratio|| '16:9',
+        imageSize:'1K',
+       },
+       safetySettings:[
+        {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.OFF,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.OFF,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold: HarmBlockThreshold.OFF,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.OFF,
+  },
+       ]
+    }
 
     
+
   } catch (error) {
-    
+
   }
 }
