44fe7b90bb5e1c3b30625acc5371bd9ea2570287
anuj9631 <anuj98426@gmail.com>
2026-02-15 14:22:11 +0530

    fixed controoler

--- server/controllers/UserController.ts
+++ server/controllers/UserController.ts
@@ -1,37 +1,27 @@
-
 import { Request, Response } from "express"
 import Thumbnail from "../models/Thumbnail.js";
-// cotrollers to get all user thumbnails 
 
 export const getUsersThumbnails = async (req: Request, res: Response) => {
   try {
-    
-    const {userId} = req.session;
-
+    const {userId} = req.session as any;
     const thumbnail = await Thumbnail.find({userId}).sort({createdAt: -1})
     res.json({thumbnail})
-
   } catch (error: any) {
     console.log(error);
-    res.json(500).json({message: error.message});
+    // Fixed: Changed res.json(500) to res.status(500)
+    res.status(500).json({message: error.message});
   }
 }
 
-// cotrollers to get a single user thumbnails 
-
-
 export const getThumbnailbyId = async (req: Request, res: Response) => {
   try {
-    
-    const {userId} = req.session;
+    const {userId} = req.session as any;
     const {id} = req.params;
-
-
     const thumbnail = await Thumbnail.findOne({userId, _id: id});
     res.json({thumbnail})
-
   } catch (error: any) {
     console.log(error);
-    res.json(500).json({message: error.message});
+    // Fixed: Changed res.json(500) to res.status(500)
+    res.status(500).json({message: error.message});
   }
 }
